ff0d861392a6359fca8d7006db42a365faec9581
mdtalham <mohmmad.talha@outlook.com>
2020-06-14 07:28:51 +0530

    Added Day1, Sample, and HelloWorld.cs

---
+++ Day1/Samples/HelloWorld.cs
@@ -0,0 +1,21 @@
+//Those are the namespaces that are (by default) included
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+//By default visual studio creates already a namespace
+namespace HelloWorld
+{
+	//Remember: Everything (variables, functions) has to be encapsulated
+	class Program
+	{
+		//The entry point is called Main (capital M) and has to be static
+		static void Main(string[] args)
+		{
+			//Writing something to the console is easily possible using the
+			//class Console with the static method Write or WriteLine
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
